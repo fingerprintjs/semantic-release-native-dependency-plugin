024da2b7d2ce4ed62e4048398d3aba07095e4036
Eray Aydın <eray.aydin@fingerprint.com>
2025-03-10 11:18:32 +0300

    test: improve generateNotes test coverage
    Refactored `index.test.ts` to improve test coverage.

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,9 +1,40 @@
 import { describe, expect, it } from '@jest/globals'
+import PluginConfig from '../src/@types/pluginConfig'
+import { GenerateNotesContext } from 'semantic-release'
 
-const Index = require('../src/index')
+const { generateNotes } = require('../src/index')
+
+const cwd = process.cwd()
+const pluginConfig = {
+  androidPath: 'android',
+  androidGradleTaskName: 'print',
+  iOSPodSpecJsonPath: 'podspec.json',
+} satisfies PluginConfig
+const generateNotesContext = {
+  cwd: cwd,
+} satisfies Pick<GenerateNotesContext, 'cwd'>
 
 describe('index', () => {
-  it('generateNotes is a function', () => {
-    expect(Index.generateNotes).toBeInstanceOf(Function)
+  describe('generateNotes', () => {
+    it('is a function', () => {
+      expect(generateNotes).toBeInstanceOf(Function)
+    })
+    it('throws error on cwd not set', async () => {
+      const testGenerateNotesContext = {
+        ...generateNotesContext,
+        cwd: undefined,
+      }
+      await expect(generateNotes(pluginConfig, testGenerateNotesContext)).rejects.toThrowErrorMatchingSnapshot('noCwd')
+    })
+    it('throws error on androidGradleTaskName not set', () => {
+      const testPluginConfig = {
+        ...pluginConfig,
+        androidGradleTaskName: undefined,
+      }
+
+      expect(generateNotes(testPluginConfig, generateNotesContext)).rejects.toThrowErrorMatchingSnapshot(
+        'noAndroidGradleTaskName'
+      )
+    })
   })
 })
