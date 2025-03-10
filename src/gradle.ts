cd53c2214aa1c56c03defd7875ca41c9eed1a10c
Eray Aydın <eray.aydin@fingerprint.com>
2025-03-10 12:40:45 +0300

    feat: add gradle system-wide availability check
    Added a new `isGradleAvailable` function to check if gradle
    installed system-wide by executing version check. Updated
    the `getCommand` function to resolve system-wide gradle if its
    only exist. Included new test cases.

--- src/gradle.ts
+++ src/gradle.ts
@@ -1,5 +1,25 @@
 import { join } from 'node:path'
 import { access, constants } from 'node:fs'
+import { exec } from 'node:child_process'
+
+/**
+ * Check if gradle is installed to system-wide.
+ * @return A promise that resolves if gradle is found
+ */
+export function isGradleAvailable(): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    exec('gradle --version', (err) => {
+      if (err) {
+        if (err.code === 127) {
+          resolve(false)
+        }
+        reject(err)
+      }
+
+      resolve(true)
+    })
+  })
+}
 
 /**
  * @param {string} cwd the path of current working directory
@@ -8,10 +28,14 @@ import { access, constants } from 'node:fs'
 export function getCommand(cwd: string): Promise<string> {
   return new Promise<string>((resolve, reject) => {
     const gradleWrapper = join(cwd, 'gradlew')
-    access(gradleWrapper, constants.F_OK, (err) => {
+    access(gradleWrapper, constants.F_OK, async (err) => {
       if (err) {
         if (err.code === 'ENOENT') {
-          resolve('gradle')
+          if (await isGradleAvailable()) {
+            resolve('gradle')
+          }
+
+          reject(new Error('Gradle or Gradle wrapper not found.'))
         } else {
           reject(err)
         }
--- test/gradle.test.ts
+++ test/gradle.test.ts
@@ -5,10 +5,35 @@ import { getCommand } from '../src/gradle'
 
 describe('Test for gradle handling', () => {
   describe('getCommand function', () => {
-    it("returns 'gradle' when there is no gradle wrapper", async () => {
+    it("returns 'gradle' if gradle is available and gradle wrapper is not found", async () => {
+      const execMock = jest.spyOn(require('node:child_process'), 'exec')
+      execMock.mockImplementation((command, callback) => {
+        if (command === 'gradle --version') {
+          // @ts-ignore
+          callback(null)
+        }
+      })
+
       const command = await getCommand(cwd())
       expect(command).toBe('gradle')
+
+      execMock.mockRestore()
     })
+
+    it('should throw error if neither gradle nor gradle wrapper are found', async () => {
+      const execMock = jest.spyOn(require('node:child_process'), 'exec')
+      execMock.mockImplementation((command, callback) => {
+        if (command === 'gradle --version') {
+          // @ts-ignore
+          callback({ code: 127 })
+        }
+      })
+
+      await expect(getCommand(cwd())).rejects.toThrowErrorMatchingSnapshot('noGradleNoWrapper')
+
+      execMock.mockRestore()
+    })
+
     it('finds the wrapper', async () => {
       const gradleProjectWithWrapper = join(cwd(), 'test/project/with-wrapper')
       const command = await getCommand(gradleProjectWithWrapper)
